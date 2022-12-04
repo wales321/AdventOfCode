60193c316aafd78be2898a18e579794472f109c0
Tom <tom.ford.wales@gmail.com>
2022-12-04 11:45:10 +0000

    Day 4

---
+++ Day4/ElfPair.cs
@@ -0,0 +1,38 @@
+﻿namespace Day4
+{
+    public class ElfPair
+    {
+        public ElfPair(string pair)
+        {
+            var pairSplit = pair.Split(',');
+            var firstElfSection = pairSplit[0].Split('-');
+            var secondElfSection = pairSplit[1].Split('-');
+
+            FirstElf = new Range(firstElfSection);
+            SecondElf = new Range(secondElfSection);
+        }
+
+        public Range FirstElf { get; }
+
+        public Range SecondElf { get; }
+
+        internal bool FullyContainsAssignments()
+        {
+            if (FirstElf.Min >= SecondElf.Min && FirstElf.Max <= SecondElf.Max)
+                return true;
+
+            if (SecondElf.Min >= FirstElf.Min && SecondElf.Max <= FirstElf.Max)
+                return true;
+
+            return false;
+        }
+
+        internal bool ContainsOverlaps()
+        {
+            var firstRange = Enumerable.Range(FirstElf.Min, (FirstElf.Max - FirstElf.Min) + 1);
+            var secondRange = Enumerable.Range(SecondElf.Min, (SecondElf.Max - SecondElf.Min) + 1);
+
+            return firstRange.Intersect(secondRange).Any();
+        }
+    }
+}
---
+++ Day4/Range.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class Range
+    {
+        public Range(string[] sectionRange)
+        {
+            Min = int.Parse(sectionRange[0]);
+            Max = int.Parse(sectionRange[1]);
+        }
+
+        public int Min { get; }
+
+        public int Max { get; }
+    }
+}
